97f0d1c17988ffc1f3bb654d9a41a3f8eff7df48
Shubham-Tyagi-Epam <shubham_tyagi@epam.com>
2022-04-04 19:52:04 +0530

    express added

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-const http = require("http");
+// const http = require("http");
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/html");
+//   res.end("<h1>Hello World</h1>");
+// });
+
+// server.listen(port, () => {
+//   console.log(`Server running at port ` + port);
+// });
+
+var express = require("express");
+var app = express();
+
+app.get("/", (req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Hello World</h1>");
+  res.end("<h1>Main Page</h1>");
 });
 
-server.listen(port, () => {
-  console.log(`Server running at port ` + port);
+app.get("/employee", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>Employee Page</h1>");
+});
+
+app.listen(port, function () {
+  console.log("Listening at " + port);
 });
